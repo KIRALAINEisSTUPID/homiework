9f0a72fa2ef42421da37dbb244f93eb1d755f228
KIRALAINEisSTUPID <169078556+KIRALAINEisSTUPID@users.noreply.github.com>
2024-10-20 23:23:54 +0500

    upddates again to set  path

--- Homiework/wsgi.py
+++ Homiework/wsgi.py
@@ -4,11 +4,11 @@ import os
 import sys
 
 # assuming your Django settings file is at '/home/myusername/mysite/mysite/settings.py'
-path = '/home/myusername/mysite'
+path = '/home/Kiralaineeeeeeeeeee/homiework'
 if path not in sys.path:
     sys.path.insert(0, path)
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'Homiework.settings'
 
 ## Uncomment the lines below depending on your Django version
 ###### then, for Django >=1.5:
