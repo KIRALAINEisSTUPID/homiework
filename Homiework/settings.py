8178306272eb1d9b5195627a6e8d0fe6a753d2c8
KIRALAINEisSTUPID <169078556+KIRALAINEisSTUPID@users.noreply.github.com>
2024-10-20 23:29:53 +0500

    allowed to hosts

--- Homiework/settings.py
+++ Homiework/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*25@g935h=0!)f)%9__yo(q6$xaf6v05gdfn#+y6l1xqn5b%ha
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
